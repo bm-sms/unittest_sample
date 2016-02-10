ddc698d5a4ca81b0333acf54c01069f8b4947475
Yuka Kato <satoimo.to.imo@gmail.com>
2016-02-10 12:57:21 +0900

    Add theme files of reverse polish calculating

---
+++ step_2/calculator.rb
@@ -0,0 +1,30 @@
+class Calculator
+  OPERAND_REGEXP  = /\d+/
+  OPERATOR_REGEXP = /\+|\*/
+
+  def initialize
+    @stack = []
+  end
+
+  def calc(expression)
+    expression.split(' ').each do |str|
+      case str
+      when OPERAND_REGEXP
+        @stack.push str.to_i
+      when OPERAND_REGEXP
+        calculate! str.to_sym
+      end
+    end
+
+    @stack.pop
+  end
+
+  private
+
+  def calculate!(operator)
+    register = @stack.pop
+    accumlator = register.send(operator, @stack.pop)
+
+    @stack.push accumlator
+  end
+end
