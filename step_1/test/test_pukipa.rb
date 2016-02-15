9df73323b940ac780f36282c1db5b2cd36cabe3e
Yuka Kato <satoimo.to.imo@gmail.com>
2016-02-15 14:20:38 +0900

    Check to parse heading

--- step_1/test/test_pukipa.rb
+++ step_1/test/test_pukipa.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require_relative '../pukipa.rb'
 
 class TestPukipa < Minitest::Test
-  def test_h_parse
+  def test_to_html
+    pu = Pukipa.new('* 見出し')
+    assert_equal '<h2>見出し</h2>', pu.to_html
   end
 end
