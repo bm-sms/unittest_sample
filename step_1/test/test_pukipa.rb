a91f6043558e046a1afbf48664285554b011a3f9
Yuka Kato <satoimo.to.imo@gmail.com>
2016-02-15 14:52:14 +0900

    Change method name

--- step_1/test/test_pukipa.rb
+++ step_1/test/test_pukipa.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative '../pukipa.rb'
 
 class TestPukipa < Minitest::Test
-  def test_to_html
+  def test_to_html_should_parse_header_element
     pu = Pukipa.new('* 見出し')
     assert_equal '<h2>見出し</h2>', pu.to_html
   end
