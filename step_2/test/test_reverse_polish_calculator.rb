97eddf973fa7bcff2fffe6ad6579d4875b0f4e2e
Yuka Kato <satoimo.to.imo@gmail.com>
2016-02-15 18:25:50 +0900

    Check calculation with plus operator

--- step_2/test/test_reverse_polish_calculator.rb
+++ step_2/test/test_reverse_polish_calculator.rb
@@ -2,5 +2,11 @@ require 'minitest/autorun'
 require_relative '../calculator.rb'
 
 class TestReversePolishCalculator < Minitest::Test
+  def setup
+    @calculator = Calculator.new
+  end
 
+  def test_calculator_should_calculate_with_plus_operator
+    assert_equal 3, @calculator.calc('1 2 +')
+  end
 end
