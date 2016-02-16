541330b2e5dba419d3b6dda0c5a9f59b31421c6f
Yuka Kato <satoimo.to.imo@gmail.com>
2016-02-16 12:20:08 +0900

    Check the installatio

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 gem install rake
 gem install minitest
 gem install minitest-reporters
+
+# Check the gems have been installed successfully
+# If the message "rake test # Run tests" is shown at the last line, your installation has been succeeded.
+cd step_1/
+rake -T
